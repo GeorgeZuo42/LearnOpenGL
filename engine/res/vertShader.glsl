d18d500b019443c394a05d33c45b28733833373b
Enak <enakzuo@123u.com>
2020-05-13 16:57:03 +0800

    Instancing渲染多个cube

--- engine/res/vertShader.glsl
+++ engine/res/vertShader.glsl
@@ -1,9 +1,65 @@
 #version 430
 layout (location=0) in vec3 position;
-uniform mat4 mv_matrix;
+uniform mat4 m_matrix; // these are now separate model and view matrices
+uniform mat4 v_matrix;
 uniform mat4 proj_matrix;
+uniform float tf;
 out vec4 varingColor;
+
+mat4 buildRotateX(float rad); // declaration of matrix transformation utility functions
+mat4 buildRotateY(float rad); // (GLSL requires functions to be declared prior to invocation)
+mat4 buildRotateZ(float rad);
+mat4 buildTranslate(float x, float y, float z);
+
 void main(void){ 
+    float i = gl_InstanceID + tf; // value based on time factor, but different for each cube instance
+    float a = sin(203.0 * i/8000.0) * 403.0;
+    float b = cos(301.0 * i/4001.0) * 401.0;
+    float c = sin(400.0 * i/6003.0) * 405.0;
+    // build the rotation and translation matrices to be applied to this cube’s model matrix
+    mat4 localRotX = buildRotateX(1000*i);
+    mat4 localRotY = buildRotateY(1000*i);
+    mat4 localRotZ = buildRotateZ(1000*i);
+    mat4 localTrans = buildTranslate(a,b,c);
+    // build the model matrix and then the model-view matrix
+    mat4 newM_matrix = m_matrix * localTrans * localRotX * localRotY * localRotZ;
+    mat4 mv_matrix = v_matrix * newM_matrix;
     gl_Position = proj_matrix * mv_matrix * vec4(position,1.0);
     varingColor = vec4(position,1.0)*0.5+vec4(0.5,0.5,0.5,0.5);
+}
+// utility function to build a translation matrix (from Chapter 3)
+mat4 buildTranslate(float x, float y, float z)
+{ 
+    mat4 trans = mat4(1.0, 0.0, 0.0, 0.0,
+                      0.0, 1.0, 0.0, 0.0,
+                      0.0, 0.0, 1.0, 0.0,
+                      x, y, z, 1.0 );
+    return trans;
+}
+// builds and returns a matrix that performs a rotation around the X axis
+mat4 buildRotateX(float rad)
+{ 
+    mat4 xrot = mat4(1.0, 0.0, 0.0, 0.0,
+    0.0, cos(rad), -sin(rad), 0.0,
+    0.0, sin(rad), cos(rad), 0.0,
+    0.0, 0.0, 0.0, 1.0 );
+    return xrot;
+}
+// builds and returns a matrix that performs a rotation around the Y axis
+mat4 buildRotateY(float rad)
+{ 
+    mat4 yrot = mat4(cos(rad), 0.0, sin(rad), 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    -sin(rad), 0.0, cos(rad), 0.0,
+    0.0, 0.0, 0.0, 1.0 );
+    return yrot;
+}
+// builds and returns a matrix that performs a rotation around the Z axis
+mat4 buildRotateZ(float rad)
+{
+    mat4 zrot = mat4(cos(rad), -sin(rad), 0.0, 0.0,
+    sin(rad), cos(rad), 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, 0.0, 1.0 );
+    return zrot;
 }
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -20,12 +20,12 @@ GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // allocate variables used in display() function, so that they won’t need to be allocated during rendering
-GLuint mvLoc, projLoc;
+GLuint mLoc, projLoc, vLoc, tfLoc;
 int width, height;
 float aspect;
-float tf;
-glm::mat4 pMat, vMat, mMat, mvMat;
-glm::mat4 tMat, rMat;
+float timeFactor;
+glm::mat4 pMat, vMat, mMat;
+
 void setupVertices(void)
 { // 36 vertices, 12 triangles, makes 2x2x2 cube placed at origin
 	float vertexPositions[108] = {
@@ -53,38 +53,36 @@ void display(GLFWwindow *window, double currentTime)
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(renderingProgram);
 	// get the uniform variables for the MV and projection matrices
-	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+
+	mLoc = glGetUniformLocation(renderingProgram, "m_matrix");
+	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+	tfLoc = glGetUniformLocation(renderingProgram, "tf");
 	// build perspective matrix
 	glfwGetFramebufferSize(window, &width, &height);
 	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
-	// build view matrix, model matrix, and model-view matrix
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
-	for (size_t i = 0; i < 24; i++)
-	{
-		tf = (float)currentTime + i;
-		tMat = glm::translate(glm::mat4(1.0f),
-							  glm::vec3(sin(0.35f * tf) * 8.0f, cos(0.52f*tf) * 8.0f, sin(0.7f *tf) * 8.0f));
-		rMat = glm::rotate(glm::mat4(1.0f), 1.75f * tf, glm::vec3(0.0f, 1.0f, 0.0f));
-		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(1.0f, 0.0f, 0.0f));
-		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(0.0f, 0.0f, 1.0f));
-		// the 1.75 adjusts the rotation speed
-		mMat = tMat * rMat;
-		mvMat = vMat * mMat;
-		// copy perspective and MV matrices to corresponding uniform variables
-		glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
-		// associate VBO with the corresponding vertex attribute in the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		glEnableVertexAttribArray(0);
-		// adjust OpenGL settings and draw model
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-	}
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
+	cameraZ = 420.0f;
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	timeFactor = ((float)currentTime);
+
+	
+	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+	glUniformMatrix4fv(mLoc, 1, GL_FALSE, glm::value_ptr(mMat));
+
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glUniform1f(tfLoc, (float)timeFactor);
+	
+	// associate VBO with the corresponding vertex attribute in the vertex shader
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	// adjust OpenGL settings and draw model
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 36,10000);
 }
 GLFWwindow *createWindow(int width, int height, const char *name)
 {
@@ -126,9 +124,9 @@ GLuint createShader(const char *filePath, GLenum type)
 
 GLuint createShaderProgram()
 {
-	GLuint vShader = createShader("vertShader.glsl",
+	GLuint vShader = createShader("res/vertShader.glsl",
 								  GL_VERTEX_SHADER);
-	GLuint fShader = createShader("fragShader.glsl",
+	GLuint fShader = createShader("res/fragShader.glsl",
 								  GL_FRAGMENT_SHADER);
 	GLint linked;
 	GLuint shaderProgram = glCreateProgram();
